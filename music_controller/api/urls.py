e9ac496888818a5533cce86a1530aeda5f2eff05
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-12-18 11:26:07 -0600

    react setup and working now

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .views import RoomView
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('room', RoomView.as_view()),
-=======
     path('home', RoomView.as_view()),
->>>>>>> e1bde2f3578d074c673ff166b60ba59a4a7dae95
+
 ] 
